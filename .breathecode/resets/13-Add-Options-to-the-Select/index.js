f4bdc586decafd8ab7aaae0eb31f51739418faad
MLopez8282 <saltairpix@gmail.com>
2020-12-17 01:11:00 +0000

    update

--- .breathecode/resets/13-Add-Options-to-the-Select/index.js
+++ .breathecode/resets/13-Add-Options-to-the-Select/index.js
@@ -2,9 +2,5 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-	var select = document.querySelector("#mySelect");
-	for (let i = 0; i < countries.length; i++) {
-		mySelect.innerHTML = select;
-	}
-	// your code here
+  // your code here
 };
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,14 @@
 // Your code here
+let input = document.querySelector("#addToDo");
+input.addEventListener("keyup", addToDo);
+
+let ul = document.querySelector("ul");
+
+const addToDo = event => {
+	if (event.keyCode === 13) {
+		let task = event.target.value;
+		ul.innerHTML += `<li>
+					<span><i class="fa fa-trash"></i></span> ${task}
+				</li>`;
+	}
+};
