aca8b5a0aa1148b3df370eaa097178f5a580d134
MLopez8282 <saltairpix@gmail.com>
2020-12-20 00:41:31 +0000

    update

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,22 +1,24 @@
 // Your code here
 let input = document.querySelector("#addToDo");
-input.addEventListener("keyup", addTodo);
-let ul = document.querySelector("ul");
-function deleteTodo(event) {
-	var tasks = document.querySelectorAll(".fa-trash");
-	tasks.forEach((item, index) => {
+let list = document.querySelector("ul");
+
+input.addEventListener("keyup", function(event) {
+	if (event.keyCode === 13) {
+		let newItem = document.createElement("LI");
+		newItem.innerHTML = `<span><i class="fa fa-trash"></i></span>${input.value}`;
+		list.appendChild(newItem);
+		input.value = "";
+		deleteTask();
+	}
+});
+
+const deleteTask = () => {
+	let tasks = document.querySelectorAll(".fa");
+	tasks.forEach(item => {
 		item.addEventListener("click", function(event) {
-			let deletedItem = event.target.parentNode.parentNode;
+			let deletedItem = item.parentNode.parentNode;
 			deletedItem.parentNode.removeChild(deletedItem);
 		});
 	});
-}
-function addTodo(event) {
-	if (event.keyCode == 13) {
-		let task = event.target.value;
-		ul.innerHTML += `<li><span><i class="fa fa-trash"></i></span> ${task}</li>`;
-		deleteTodo();
-		console.log(event);
-	}
-}
-deleteTodo();
+};
+deleteTask();
